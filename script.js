29382ede228c2ec0a1a69ce263840d07e9bb4548
Prashant Sengar <64002012+prashant-s99@users.noreply.github.com>
2023-08-20 01:45:06 +0530

    Update Script

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const chatgpt = document.querySelector('.chatgpt');
 const bard = document.querySelector('.bard');
 const huggingchat = document.querySelector('.huggingchat')
 
-const overlay = document.querySelector('.overlay');
+const showOverlay = ()=> {
+    document.querySelector('.overlay').classList.remove('hidden')
+};
+const hideOverlay = ()=> document.querySelector('.overlay').classList.add('hidden');
 
 const closeChatGPT = document.querySelector('.chatgptcl');
 const closeBard = document.querySelector('.bardcl');
@@ -12,27 +15,27 @@ const closeHuggingChat = document.querySelector('.huggingchatcl');
 
        document.querySelector('.btn-chatGPT').addEventListener('click',function(){
             chatgpt.classList.remove('hidden');
-            overlay.classList.remove('hidden');
+            showOverlay();
         });
         document.querySelector('.btn-bard').addEventListener('click',function(){
             bard.classList.remove('hidden');
-            overlay.classList.remove('hidden');
+            showOverlay();
         });
         document.querySelector('.btn-huggingchat').addEventListener('click',function(){
             huggingchat.classList.remove('hidden');
-            overlay.classList.remove('hidden');
+            showOverlay();
         });
 
 
         closeChatGPT.addEventListener('click',function(){
             chatgpt.classList.add('hidden');
-            overlay.classList.add('hidden');
+            hideOverlay();
         });
         closeBard.addEventListener('click',function(){
             bard.classList.add('hidden');
-            overlay.classList.add('hidden');
+            hideOverlay();
         });
         closeHuggingChat.addEventListener('click',function(){
             huggingchat.classList.add('hidden');
-            overlay.classList.add('hidden');
+            hideOverlay();
         });
