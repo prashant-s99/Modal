2685cf72bdf9e8d0417c71afb2480701eddd4a48
Prashant Sengar <64002012+prashant-s99@users.noreply.github.com>
2023-08-21 23:25:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,35 +42,14 @@ const closeHuggingChat = document.querySelector('.huggingchatcl');
         });
 
         //'Esc' keypress event listener
-        document.addEventListener('keydown', function(event){
-            if(event.key==='Escape')
-            {
-                // {
-                //     if(!(chatgpt.classList.contains('hidden')))
-                //     {
-                //         chatgpt.classList.add('hidden');
-                //         hideOverlay();
-                //     }
-                //     else if(!(bard.classList.contains('hidden')))
-                //     {
-                //         bard.classList.add('hidden');
-                //         hideOverlay();
-                //     }
-                //     else if(!(huggingchat.classList.contains('hidden')))
-                //     {
-                //         huggingchat.classList.add('hidden');
-                //         hideOverlay();
-                //     }
-                // }
-
-                //Looping on elements having a common class name and checking wheather
+        document.addEventListener('keydown', function(event){                
+                //Looping on all elements having a common class name 'modal' and checking wheather
                     for(let i=0; i<mod.length; i++)
                     {
-                        if(!mod[i].classList.contains('hidden'))
+                        if(event.key==='Escape' && !mod[i].classList.contains('hidden'))
                         {
                         mod[i].classList.add('hidden');
                         hideOverlay();
                         }
                     }
-            }
         });
